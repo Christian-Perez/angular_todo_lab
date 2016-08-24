a9ec42ffc59a73c1cb00b24ac326dcce08fab21e
Christian Perez <chris.dillon92@gmail.com>
2016-08-24 11:58:42 -0700

    adds list of todos >> make form

--- js/todoController.js
+++ js/todoController.js
@@ -2,5 +2,12 @@ angular.module('todoApp')
   .controller('TodoController', TodoController)
 
 function TodoController(){
+  var self = this
+  self.newTodo = {title: '', content: '', completed: false}
+  self.todos = [
+    {title: 'walk the dishes', content: 'do it', completed: false},
+    {title: 'wash the laundry', content: 'just do it', completed: false},
+    {title: 'brush the toilet', content: 'come on, do it already', completed: false}
+  ]
   
 }
